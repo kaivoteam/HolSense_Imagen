884f88619763be6ece9f4edc9ba72e96043fd480
Francisco <pansho.del.18@hotmail.com>
2017-07-25 17:11:37 -0400

    Movimientos de imagen

--- 4caras_frame.py
+++ 4caras_frame.py
@@ -187,30 +187,44 @@ print "La imagen tiene %d frames"%(frames)
 
 #4 imagenes en 4 angulos
 angulos = [0.0, 90.0, 180.0, 270.0]
-#Movimiento de la imagen a traves de current
-current = 0
+
+
+tamanno_original = im.size[0] #para imagenes cuadradas
 
 #caracteristicas de calibracion
 aspecto_normal = 0.2 # o 1/5
 
+#Movimiento de la imagen a traves de actual
+current = 0 #frame en el momento (actual)
+zoom = 1.0  #zoom en el momento (actual)
+
 while(True):
-    movimiento = raw_input("1 Derecha \n2 Izquierda \n")
+    opcion = raw_input("1 Derecha \n2 Izquierda \n3 Zoom in \n4 Zoom out\n")
+
+    #tamanno_actual = int(tamanno_original*aspecto_normal*zoom)
 
     #asignar cantidad de mov
 
     start_time = time.time()
 
-    if movimiento == '1': #derecha
+    if opcion == '1': # movimiento derecha
         current +=1 %frames
-    elif movimiento == '2': #izquierda
+
+    elif opcion == '2': #movimiento izquierda
         current -=1 %frames
+        if current <0: #ya que solo va en numeros positivos
+            current += frames
+
+    elif opcion == '3': #hacer zoom
+        zoom+=0.1 #calibrar
+
+    elif opcion == '4': #quitar zoom
+        zoom-=0.1
     else:
         print("Movimiento invalido")
         continue
 
-    if current <0: #ya que solo va en numeros positivos
-        current += frames
-
+    ##-----------------CREAR LA IMAGEN---------------
     caras = []
     for angulo in angulos: #extraer 4 angulos a partir del current
         frame_angulo = (angulo_a_frame(angulo, frames )+current) % frames
@@ -223,35 +237,49 @@ while(True):
     w,h = 920,512#caras[0].size
     mascara = Image.new('RGB', (w,h))
 
-    print "hola mundo"
-                            
-    #achicarlos 1/5 del tamanno
-    size = int(w*aspecto_normal)
-    caras[0].thumbnail((size,size)) #cara frente
+    #nuevo tamanno
+    tamanno_actual = int(tamanno_original*aspecto_normal*zoom)
+
+    caras[0].thumbnail((tamanno_actual,tamanno_actual),Image.ANTIALIAS) #cara frente
     cara_frente = caras[0].rotate(180)#,expand=True)
-    caras[1].thumbnail((size,size)) #cara izquierda
+    caras[1].thumbnail((tamanno_actual,tamanno_actual),Image.ANTIALIAS) #cara izquierda
     cara_izquierda = caras[1].rotate(90)#,expand=True)
-    caras[2].thumbnail((size,size)) #cara derecha
+    caras[2].thumbnail((tamanno_actual,tamanno_actual),Image.ANTIALIAS) #cara derecha
     cara_derecha = caras[2].rotate(270)#,expand=True)
-    caras[3].thumbnail((size,size)) #cara atras
+    caras[3].thumbnail((tamanno_actual,tamanno_actual),Image.ANTIALIAS) #cara atras
     cara_atras = caras[3].rotate(0)#,expand=True)
+
+    ##---------------------AJUSTAR--------------------------
+    if zoom > 1: #si se sale de los limites del ratio base
+        #imagenes cuadradas
+        mitad = tamanno_actual/2
+
+        #tamanno seria de tamanno_actual*aspecto_normal para manternerlo
+        tamanno = int(tamanno_original*aspecto_normal)
+        x1 = x2= mitad - tamanno/2
+        y1 = y2 = mitad + tamanno/2
+
+        cara_frente = cara_frente.crop((x1, x2, y1, y2))
+        cara_izquierda = cara_izquierda.crop((x1, x2, y1, y2))
+        cara_derecha = cara_derecha.crop((x1, x2, y1, y2))
+        cara_atras = cara_atras.crop((x1, x2, y1, y2))
+
+
+    dimension = cara_frente.size[0]#para juntar imagenes
 
     #calibrar todo esto
     #distancia en width
-    pos_y_frente = h- h/4 - size/2  #-size/2 para centrar
+    pos_y_frente = h- h/4 - dimension/2  #-dimension/2 para centrar
     dist_y = h/2
     #distancia en height
-    pos_x_der = w - w/3 - size/2
+    pos_x_der = w - w/3 - dimension/2
     dist_x = w/3
 
 
-    mascara.paste(cara_frente, ( (w-size)/2, pos_y_frente))
-    mascara.paste(cara_izquierda, ( pos_x_der - dist_x , (h - size)/2))
-    mascara.paste(cara_derecha, (pos_x_der , (h - size)/2))         
-    #if rotacion == 1:
-    #   mascara.paste(caras[1], (w-w/3, h/2)) #cara izquierda
-    #   mascara.paste(caras[2], (w/3, h/2))   #cara derecha
-    mascara.paste(cara_atras, ( (w-size)/2, pos_y_frente - dist_y))   
+    mascara.paste(cara_frente, ( (w-dimension)/2, pos_y_frente))
+    mascara.paste(cara_izquierda, ( pos_x_der - dist_x , (h - dimension)/2))
+    mascara.paste(cara_derecha, (pos_x_der , (h - dimension)/2))         
+    mascara.paste(cara_atras, ( (w-dimension)/2, pos_y_frente - dist_y))   
 
     #mostrar y guardar
     mascara.show()
@@ -260,78 +288,18 @@ while(True):
     #im.show()
     print "Demoro %f segundos en total"%(time.time() - start_time)
 
-
-    ##zoom
-    zoom = raw_input("1 zoom in \n2 zoom out \n")
-
-    w,h = 920,512#caras[0].size
-    mascara = Image.new('RGB', (w,h))
-                            
-    #aplicar zoom
-    #definir un maximo pequenno y grande
-    if zoom == '1':
-        size = w/4
-        #y crop...
-        #100px * 100px, starting centro
-        #half_the_width = caras[0].size[0] / 2
-        #half_the_height = caras[0].size[1] / 2
-
-        #tamanno seria de size*aspecto_normal para manternerlo
-
-        #img2 = img.crop((0, 0, 100, 100))
-
-        #para imagenes no cuadradas
-        """ Pad the image to a square
-        longer_side = max(img.size)
-        horizontal_padding = (longer_side - img.size[0]) / 2
-        vertical_padding = (longer_side - img.size[1]) / 2
-        img5 = img.crop(
-            (
-                -horizontal_padding,
-                -vertical_padding,
-                img.size[0] + horizontal_padding,
-                img.size[1] + vertical_padding
-            )
-        )
-
-"""
-    elif zoom == '2':
-        size=w/7
-    else:
-        print("Zoom invalido")
-        continue
-
-    caras = []
-    for angulo in angulos: #extraer 4 angulos a partir del current
-        frame_angulo = (angulo_a_frame(angulo, frames )+current) % frames
-        im.seek(frame_angulo-1)
-
-        caras.append(im.copy())
-
-    caras[0].thumbnail((size,size),Image.ANTIALIAS) #cara frente
-    print cara_frente.size
-    print size
-    cara_frente = caras[0].rotate(180)#,expand=True)
-    caras[1].thumbnail((size,size),Image.ANTIALIAS) #cara izquierda
-    cara_izquierda = caras[1].rotate(90)#,expand=True)
-    caras[2].thumbnail((size,size),Image.ANTIALIAS) #cara derecha
-    cara_derecha = caras[2].rotate(270)#,expand=True)
-    caras[3].thumbnail((size,size),Image.ANTIALIAS) #cara atras
-    cara_atras = caras[3].rotate(0)#,expand=True)
-
-    #calibrar todo esto
-    #distancia en width
-    pos_y_frente = h- h/4 - size/2  #-size/2 para centrar
-    dist_y = h/2
-    #distancia en height
-    pos_x_der = w - w/3 - size/2
-    dist_x = w/3
-
-    mascara.paste(cara_frente, ( (w-size)/2, pos_y_frente))
-    mascara.paste(cara_izquierda, ( pos_x_der - dist_x , (h - size)/2))
-    mascara.paste(cara_derecha, (pos_x_der , (h - size)/2))         
-    mascara.paste(cara_atras, ( (w-size)/2, pos_y_frente - dist_y))   
-
-    #mostrar y guardar
-    mascara.show()
-
+   
+#size = w/4
+#para imagenes no cuadradas
+""" Pad the image to a square
+longer_side = max(img.size)
+horizontal_padding = (longer_side - img.size[0]) / 2
+vertical_padding = (longer_side - img.size[1]) / 2
+img5 = img.crop(
+    (
+        -horizontal_padding,
+        -vertical_padding,
+        img.size[0] + horizontal_padding,
+        img.size[1] + vertical_padding
+    )
+)"""
